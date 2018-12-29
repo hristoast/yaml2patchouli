fc29104665ca9ce537ba4634c4b4a27da6c93fb0
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2018-12-28 18:23:13 -0600

    Smoothing things out, adding several features
    * Users will be shown an error message if python3-yaml is absent
      rather than a python traceback.
    * The version number is now set in the app itself.
    * create_book_dirs() didn't create sub dirs for entries, now it does.
    * create_book_dirs() also now gracefully handles when the given
      directory is not writeable for any reason.
    * Handle when the given yaml file isn't a yaml file or even a file at
      all.
    * Added a function to abstract formatting category and entry file
      names.
    * write_thing() now accounts for sub dirs on entries.
    * It also now has a no-op logic branch for when templates are
      supported.
    * The logging wrapper now dynamically gets the log level.
    * Added several new cli options, and bits to go with them in main():
      * --version which should be obvious.
      * --clean -- erases the book directory before generating
      * --verbose -- doesn't eliipsize the output text when the terminal
        is too narrow.
    * Each category and entry that is going to be generated will be listed
      in the output now.
    * Other output tweaks and etc.

--- y2p/core.py
+++ y2p/core.py
@@ -3,30 +3,55 @@ import argparse
 import json
 import logging
 import os
+import shutil
 import sys
 import textwrap
-import yaml
-
-
+PROG = sys.argv[0].split('/')[-1]
+try:
+    import yaml
+except ImportError:
+    sys.stderr.write("The python3-yaml package is required to use {}!\n".format(PROG))
+    sys.exit(1)
 DESC = "Create Patchouli books from a single YAML file."
 LOGFMT = '%(asctime)s | %(message)s'
-PROG = sys.argv[0].split('/')[-1]
+__version__ = "0.2"
 
 
-def create_book_dirs(name: str, lang: str, out: str) -> None:
-    book_dir = os.path.join(out, "patchouli_books", name)
+def create_book_dirs(book_name: str, entries: list, lang: str, out: str) -> None:
+    book_dir = os.path.join(out, "patchouli_books", book_name)
+
     for path in os.path.join(book_dir, lang, "categories"), \
-            os.path.join(book_dir, lang, "entries"):
+            os.path.join(book_dir, lang, "entries"), \
+            os.path.join(book_dir, lang, "templates"):
         try:
             os.makedirs(path)
+        except FileExistsError:
+            # It already exists, cool.
+            pass
+        except PermissionError:
+            error_and_die("The given out dir is not writeable!")
+
+    for e in entries:
+        entry_name = format_name(e["name"])
+        entry_dir = os.path.join(book_dir, lang, "entries", entry_name)
+        try:
+            os.makedirs(entry_dir)
         except FileExistsError:
             pass
 
 
 def read_yaml(yaml_file: str) -> str:
-    with open(yaml_file, 'r') as f:
-        lines = f.readlines()
-    return ' '.join(lines)
+    try:
+        with open(yaml_file, 'r') as f:
+            lines = f.readlines()
+        return ' '.join(lines)
+    except FileNotFoundError:
+        error_and_die("The given YAML file ({}) was not a real file!".format(yaml_file))
+
+
+def format_name(name: str) -> str:
+    name = name.replace(" ", "_")
+    return ''.join(a for a in name if a.isalnum() or a == "_").lower()
 
 
 def write_book_json(name: str, data: str, out: str) -> None:
@@ -37,13 +62,17 @@ def write_book_json(name: str, data: str, out: str) -> None:
         f.write("\n")
 
 
-def write_thing(thing: dict, type: str, lang: str, name: str, out: str):
+def write_thing(thing: dict, type: str, lang: str, name: str, out: str) -> None:
     if type == "category":
-        file_name = ''.join(a for a in thing["name"] if a.isalnum()) + "_category.json"
+        file_name = format_name(thing["name"]) + "_category.json"
         path = os.path.join(out, "patchouli_books", name, lang, "categories", file_name)
     elif type == "entry":
-        file_name = ''.join(a for a in thing["name"] if a.isalnum()) + "_entry.json"
-        path = os.path.join(out, "patchouli_books", name, lang, "entries", file_name)
+        entry_name = format_name(thing["name"])
+        file_name = entry_name + "_entry.json"
+        path = os.path.join(out, "patchouli_books", name, lang, "entries", entry_name, file_name)
+    elif type == "template":
+        # TODO: support macros/templates
+        print("TODO")
     with open(path, 'w+') as f:
         for line in json.dumps(thing, indent=4, sort_keys=True):
             f.write(line)
@@ -60,8 +89,11 @@ def get_terminal_dims() -> tuple:
     return x, y
 
 
-def emit_log(msg: str, level=logging.INFO, *args, **kwargs) -> None:
+def emit_log(msg: str, level=None, *args, **kwargs) -> None:
     """Logging wrapper."""
+    if not level:
+        logger = logging.getLogger()
+        level = logger.level
     if not level == logging.DEBUG and int(get_terminal_dims()[0]) > 0:
         _num = 31  # magic number to get how many actual columns we have to work with
         msg = textwrap.shorten(msg, width=int(get_terminal_dims()[0]) - _num,
@@ -92,36 +124,51 @@ def parse_argv() -> None:
     """Set up args and parse them."""
     parser = argparse.ArgumentParser(description=DESC, prog=PROG)
     parser.add_argument("yaml_file", help="YAML file to be read.", metavar="<YAML file>")
+    parser.add_argument("--version", action="version", version=PROG + " " + __version__, help=argparse.SUPPRESS)
     options = parser.add_argument_group("Options")
+    options.add_argument("-C", "--clean", action="store_true", help="Erase the book directory before generating.")
     options.add_argument("-o", "--out-dir",
                          help="Where the book files should be written to.  Defaults to $PWD if not provided")
     options.add_argument("-q", "--quiet", action="store_true",
                          help="Don't print messages, just create the books files and directories.")
+    options.add_argument("-V", "--verbose", action="store_true", help="Don't ellipsize output, do word wrap.")
     return parser.parse_args()
 
 
 def main() -> None:
     parsed = parse_argv()
 
+    clean = False
+    log_level = logging.INFO
     out_dir = None
     quiet = False
+    verbose = False
     yaml_file = None
 
+    if parsed.clean:
+        clean = True
     if parsed.out_dir:
         out_dir = parsed.out_dir
     if parsed.quiet:
         quiet = True
+    if parsed.verbose:
+        verbose = True
     if parsed.yaml_file:
-        yaml_file = parsed.yaml_file
+        yaml_file = os.path.abspath(parsed.yaml_file)
 
     if not out_dir:
         # TODO: is this brittle?
         out_dir = os.getenv("PWD")
 
+    # Absolute paths are preferred and more explicit
+    out_dir = os.path.abspath(out_dir)
+
     if quiet:
-        init_logging(logging.FATAL)
-    else:
-        init_logging(logging.INFO)
+        log_level = logging.FATAL
+    elif verbose:
+        log_level = logging.DEBUG
+
+    init_logging(log_level)
 
     emit_log("Getting data from '" + yaml_file + "'")
     book_data = yaml.load(read_yaml(yaml_file))
@@ -140,21 +187,26 @@ def main() -> None:
     entries = book_data.get("entries")
     del book_data["entries"]
 
-    emit_log("Creating book directories in '" + out_dir + "'")
-    create_book_dirs(book_name, book_lang, out_dir)
+    book_dir = os.path.join(out_dir, "patchouli_books", book_name)
+    if clean:
+        emit_log("Removing book directories in '" + book_dir + "'")
+        shutil.rmtree(book_dir)
+
+    emit_log("Creating book directories in '" + book_dir + "'")
+    create_book_dirs(book_name, entries, book_lang, out_dir)
 
     emit_log("Writing 'book.json'")
     write_book_json(book_name, json.dumps(book_data, indent=4, sort_keys=True), out_dir)
 
-    emit_log("Writing category files")
+    emit_log("Writing category files: " + ', '.join(cat["name"] for cat in categories).rstrip(", "))
     for c in categories:
         write_thing(c, "category", book_lang, book_name, out_dir)
 
-    emit_log("Writing entry files")
+    emit_log("Writing entry files: " + ', '.join(ent["name"] for ent in entries).rstrip(", "))
     for e in entries:
         write_thing(e, "entry", book_lang, book_name, out_dir)
 
-    emit_log("Done!")
+    emit_log("Completed generation of the book '{0}' into the directory '{1}'!".format(book_name, out_dir))
 
 
 if __name__ == '__main__':
